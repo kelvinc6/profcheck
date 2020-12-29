e44fe67ba3d02c15b0e4b7a53e8d6c41d98ed03d
kelvincao <kelvincao6@gmail.com>
2020-12-28 22:39:10 -0800

    Added link to RMP page

--- background.js
+++ background.js
@@ -1,25 +1,29 @@
+
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    getRating(request.greeting).then(res => sendResponse({farewell: res}))
+    getRating(request.greeting).then(res => sendResponse(res))
     return true;
   }
 );
 
 async function getRating(instructorName) {
   const URL = "https://www.ratemyprofessors.com"
-  const searchQueryURL = `/search.jsp?queryBy=teacherName&schoolName=university+of+british+columbia&queryoption=HEADER&query=${instructorName}&facetSearch=true`
 
+  const searchQueryURL = `/search.jsp?queryBy=teacherName&schoolName=university+of+british+columbia&queryoption=HEADER&query=${instructorName}&facetSearch=true`
   const searchQueryHTML = await fetch(URL + searchQueryURL).then(res => res.text())
 
+  //These are the results obtained from performing a search for the given professor
+  //Assumes each professor has a unique name
   const listings = $(searchQueryHTML).find(".listings").children()
   const link = await $(listings).find("a").attr("href")
 
+  //Here we go to the actual page of the professor
   const professorQueryURL = URL + link
-
   const professorQueryHTML = await fetch(professorQueryURL).then(res => res.text())
 
+  //Get the rating of a professor
   const rating = $(professorQueryHTML).find("div.RatingValue__Numerator-qw8sqy-2.liyUjw").text()
 
-  return rating
+  return { rating: rating, link: professorQueryURL }
 
 }
--- content.js
+++ content.js
@@ -1,15 +1,30 @@
-//TODO: move logic to background scripts
+//Used for storing typos
+const typos = {'ALINIAEIFARD+FARID': "ALINIAIEFARD+FARID",
+'RUED+THOMAS': "RUD+THOMAS"}
 
-const formatInstructorName = string => {
+
+const formatInstructorNames = string => {
     return string.replace(/,/, "").replace(/ /g, "+")
 }
 
 const instructorRow = $("table[class=\\table] > tbody > tr > td")
-const instructorName = formatInstructorName(instructorRow.eq(1).text())
+let instructorName = formatInstructorNames(instructorRow.eq(1).text())
+
+
+//Check if SSC has a typo
+if (typos.hasOwnProperty(instructorName)) {
+    instructorName = typos[instructorName]
+}
     
 chrome.runtime.sendMessage({greeting: instructorName}, function(res) {
     console.log(res.farewell)
-    const rating = res.farewell
+    const rating = res.rating
+    const link = res.link
 
-    $("table[class=\\table] > tbody > tr").append(`<td>Rating: ${rating}</td>`)
+    const ratingHTML = `<td>Rating: ${rating} / 5</td>`
+    const linkHTML = `<td><a href='${link}'>RMP Page</a></td>`
+
+    // $("table[class=\\table] > tbody > tr").append(ratingHTML + linkHTML)
+    //TODO: change logic to use first row of table
+    $('td:contains("Instructor")').parent().append(ratingHTML + linkHTML)
 })
