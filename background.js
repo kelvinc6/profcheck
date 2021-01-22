4ce01d2b654ee867b28dd656e1f4fbabfb99cde9
kelvincao <kelvincao6@gmail.com>
2021-01-21 22:29:48 -0800

    tidied up formatting

--- background.js
+++ background.js
@@ -1,131 +1,127 @@
 //Add listener for content script message
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-        getInfo(request.instructorName, request.isUBCO).then(res => sendResponse(res))
-        return true;
-  }
-);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  getInfo(request.instructorName, request.isUBCO).then((res) =>
+    sendResponse(res)
+  );
+  return true;
+});
 
 //Get instructor information from RMP
 async function getInfo(instructorName, isUBCO) {
   //If exact search doesn't work, try a fuzzy search
-  const exactQueryURL = queryConstructor(instructorName, isUBCO)
+  const exactQueryURL = exactQueryConstructor(instructorName, isUBCO);
+  const fuzzyQueryURL = fuzzyQueryConstructor(instructorName, isUBCO);
 
-  console.log(exactQueryURL);
-  const fuzzyQueryURL = fuzzyQueryConstructor(instructorName, isUBCO)
-
-  return sendQueryRequest(exactQueryURL, fuzzyQueryURL)
+  return sendQueryRequest(exactQueryURL, fuzzyQueryURL);
 }
 
 async function sendQueryRequest(exactURL, fuzzyURL) {
-  let json = await fetch(exactURL).then(res => res.json())
+  let json = await fetch(exactURL).then((res) => res.json());
 
-  //There is a possibility of RMP having two entries of the same professor; we always take the first result, as its sorted by number of Ratings
+  //There is a possibility of RMP having two entries of the same professor; we always take the first result, as
+  //results are sorted by number of ratings
   if (json.response.numFound != 0) {
-    const professorData = json.response.docs[0]
-    const PROF_LINK = `https://www.ratemyprofessors.com/ShowRatings.jsp?tid=${professorData.pk_id}`
-
+    const professorData = json.response.docs[0];
+    const PROF_LINK = `https://www.ratemyprofessors.com/ShowRatings.jsp?tid=${professorData.pk_id}`;
     return {
       isSuccessful: true,
       averageRatingScore: professorData.averageratingscore_rf,
       numRatings: professorData.total_number_of_ratings_i,
-      link: PROF_LINK
-    }
+      link: PROF_LINK,
+    };
   } else {
-    json = await fetch(fuzzyURL).then(res => res.json())
+    json = await fetch(fuzzyURL).then((res) => res.json());
 
     //Result of fuzzy search should be 1, otherwise unsure of correct listign
     if (json.response.numFound == 1) {
-      const professorData = json.response.docs[0]
-      const PROF_LINK = `https://www.ratemyprofessors.com/ShowRatings.jsp?tid=${professorData.pk_id}`
+      const professorData = json.response.docs[0];
+      const PROF_LINK = `https://www.ratemyprofessors.com/ShowRatings.jsp?tid=${professorData.pk_id}`;
       return {
         averageRatingScore: professorData.averageratingscore_rf,
         numRatings: professorData.total_number_of_ratings_i,
-        link: PROF_LINK
-      }
+        link: PROF_LINK,
+      };
     }
   }
   return {
     isSuccessful: false,
     averageRatingScore: null,
     numRatings: null,
-    link: null
-  }
+    link: "https://www.ratemyprofessors.com/AddTeacher.jsp",
+  };
+}
 
-
-function getAndFormatFirstNameArray(instructorName) {
-  const nameArray = instructorName.split(', ')
-  const firstNameArray = nameArray[1].split(' ')
+function createFirstNameArray(instructorName) {
+  const nameArray = instructorName.split(", ");
+  const firstNameArray = nameArray[1].split(" ");
   for (i = 0; i < firstNameArray.length; i++) {
-    firstNameArray[i] = firstNameArray[i].charAt(0) + firstNameArray[i].toLowerCase().slice(1)
+    firstNameArray[i] =
+      firstNameArray[i].charAt(0) + firstNameArray[i].toLowerCase().slice(1);
   }
-  return firstNameArray
+  return firstNameArray;
 }
 
-function getAndFormatLastNameArray(instructorName) {
-  const nameArray = instructorName.split(', ')
-  const lastNameArray = nameArray[0].split(' ')
+function createLastNameArray(instructorName) {
+  const nameArray = instructorName.split(", ");
+  const lastNameArray = nameArray[0].split(" ");
   for (i = 0; i < lastNameArray.length; i++) {
-    lastNameArray[i] = lastNameArray[i].charAt(0) + lastNameArray[i].toLowerCase().slice(1)
+    lastNameArray[i] =
+      lastNameArray[i].charAt(0) + lastNameArray[i].toLowerCase().slice(1);
   }
-  return lastNameArray
+  return lastNameArray;
 }
 
-function queryConstructor(instructorName, isUBCO) {
-  const firstNameArray = getAndFormatFirstNameArray(instructorName)
-  const lastNameArray = getAndFormatLastNameArray(instructorName)
+function exactQueryConstructor(instructorName, isUBCO) {
+  const firstNameArray = createFirstNameArray(instructorName);
+  const lastNameArray = createLastNameArray(instructorName);
 
-  const schoolID = isUBCO ? '5436' : '1413'
+  const schoolID = isUBCO ? "5436" : "1413";
 
-  let databaseURL = `https://solr-aws-elb-production.ratemyprofessors.com/solr/rmp/select/?wt=json&sort=total_number_of_ratings_i+desc&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+rating_class&q=schoolid_s:${schoolID}+AND+(`
+  let databaseURL = `https://solr-aws-elb-production.ratemyprofessors.com/solr/rmp/select/?wt=json&sort=total_number_of_ratings_i+desc&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+rating_class&q=schoolid_s:${schoolID}+AND+(`;
 
   for (i = 0; i < firstNameArray.length; i++) {
     for (j = 0; j < lastNameArray.length; j++) {
-      const firstName = firstNameArray[i]
-      const lastName = lastNameArray[j]
+      const firstName = firstNameArray[i];
+      const lastName = lastNameArray[j];
 
-      const searchQuery = `(teacherlastname_t:${lastName}+AND+teacherfirstname_t:${firstName})`
-      databaseURL = databaseURL.concat(searchQuery)
+      const searchQuery = `(teacherlastname_t:${lastName}+AND+teacherfirstname_t:${firstName})`;
+      databaseURL = databaseURL.concat(searchQuery);
 
       //Close statement with closing parentheses or add an OR
       if (i === firstNameArray.length - 1 && j === lastNameArray.length - 1) {
-        databaseURL = databaseURL.concat(')')
+        databaseURL = databaseURL.concat(")");
       } else {
-        databaseURL = databaseURL.concat('+OR+')
+        databaseURL = databaseURL.concat("+OR+");
       }
     }
   }
-  return databaseURL
+  return databaseURL;
 }
 
 function fuzzyQueryConstructor(instructorName, isUBCO) {
-  const firstNameArray = getAndFormatFirstNameArray(instructorName)
-  const lastNameArray = getAndFormatLastNameArray(instructorName)
+  const firstNameArray = createFirstNameArray(instructorName);
+  const lastNameArray = createLastNameArray(instructorName);
 
-  const schoolID = isUBCO ? '5436' : '1413'
+  const schoolID = isUBCO ? "5436" : "1413";
 
-  let databaseURL = `https://solr-aws-elb-production.ratemyprofessors.com/solr/rmp/select/?wt=json&sort=total_number_of_ratings_i+desc&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+rating_class&q=schoolid_s:${schoolID}+AND+(`
+  let databaseURL = `https://solr-aws-elb-production.ratemyprofessors.com/solr/rmp/select/?wt=json&sort=total_number_of_ratings_i+desc&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+rating_class&q=schoolid_s:${schoolID}+AND+(`;
 
   for (i = 0; i < firstNameArray.length; i++) {
     for (j = 0; j < lastNameArray.length; j++) {
-      const firstName = firstNameArray[i]
-      const lastName = lastNameArray[j]
+      const firstName = firstNameArray[i];
+      const lastName = lastNameArray[j];
 
       //Searches with fuzzy first exact last, or vice versa
-      const searchQuery = `((teacherlastname_t:${lastName}+AND+teacherfirstname_t:${firstName}~)+OR+(teacherlastname_t:${lastName}~+AND+teacherfirstname_t:${firstName}))`
-      databaseURL = databaseURL.concat(searchQuery)
+      const searchQuery = `((teacherlastname_t:${lastName}+AND+teacherfirstname_t:${firstName}~)+OR+(teacherlastname_t:${lastName}~+AND+teacherfirstname_t:${firstName}))`;
+      databaseURL = databaseURL.concat(searchQuery);
 
       //Close statement with closing parentheses or add an OR
       if (i === firstNameArray.length - 1 && j === lastNameArray.length - 1) {
-        databaseURL = databaseURL.concat(')')
+        databaseURL = databaseURL.concat(")");
       } else {
-        databaseURL = databaseURL.concat('+OR+')
+        databaseURL = databaseURL.concat("+OR+");
       }
     }
-    return databaseURL
+    return databaseURL;
   }
 }
-
-
-
-
--- content.js
+++ content.js
@@ -1,71 +1,74 @@
 const typos = {
-    'BROWN, LINDSAY': 'ROGERS, LINDSAY',
-    'O\'NEILL, ANGELA': 'O\'NEILL, ANGIE'
-}
+  "BROWN, LINDSAY": "ROGERS, LINDSAY",
+  "O'NEILL, ANGELA": "O'NEILL, ANGIE",
+};
 
 //The table with class "table" has instrucors and TA names
-const table = $('table[class=\\table] > tbody').children()
+const table = $("table[class=\\table] > tbody").children();
 
 //Set up correct HTML elements to fill in
 table.each((i, elem) => {
-    const ratingElement = `<td style="min-width:10em" id="rating${i}"></td>`
-    const numRatingsElement = `<td style="min-width:10em" id="numRatings${i}"></td>`
-    const linkElement = `<td style="min-width:10em"><a href="#" id="link${i}" target="_blank"></a></td>`
+  const ratingElement = `<td style="min-width:10em" id="rating${i}"></td>`;
+  const numRatingsElement = `<td style="min-width:10em" id="numRatings${i}"></td>`;
+  const linkElement = `<td style="min-width:10em"><a href="#" id="link${i}" target="_blank"></a></td>`;
 
-    $(elem).append(ratingElement + numRatingsElement + linkElement)
-})
+  //Append the template elements into the table row
+  $(elem).append(ratingElement + numRatingsElement + linkElement);
+});
 
 //Array of searched instructors
-let searched = []
+let searched = [];
 
 //Iterate through all the instructors
 table.each((i, elem) => {
-    const isTA = $(elem).has('td:contains("TA:")').length ? true : false
-    let instructorName = $(elem).find("a").text().replace("(Coordinator)", "")
+  const isTA = !!$(elem).has('td:contains("TA:")').length;
+  let instructorName = $(elem).find("a").text().replace("(Coordinator)", "");
 
-    //Break out of loop upon reaching a TA, as instructors are listed first
-    if (isTA) {
-        return false
+  //Break out of loop upon reaching a TA, as instructors are listed first
+  if (isTA) {
+    return false;
+  }
+
+  //Continue if instructor has been seen before, or if no name present
+  if (searched.includes(instructorName) || !instructorName) {
+    return;
+  } else {
+    //Keep track of instructors iterated over
+    searched.push(instructorName);
+  }
+
+  //Scuffed typo checking
+  if (typos.hasOwnProperty(instructorName)) {
+    instructorName = typos[instructorName];
+  }
+
+  //Loading indicator
+  $(`#rating${i}`).text("Loading...");
+
+  chrome.runtime.sendMessage(
+    { isUBCO: isUBCO(), instructorName: instructorName },
+    function (result) {
+      const isSuccessful = result.isSuccessful;
+      const rating = result.averageRatingScore;
+      const link = result.link;
+      const numRatings = result.numRatings;
+
+      if (isSuccessful) {
+        //Hide loading indicator
+        $(".loader").hide();
+
+        $(`#rating${i}`).text(rating ? `Rating: ${rating} / 5` : "N/A");
+        $(`#numRatings${i}`).text(`(${numRatings} ratings)`);
+        $(`#link${i}`).text(`RMP Page`).attr("href", link);
+      } else {
+        //Indicate instructor could not be found
+        $(`#rating${i}`).text("No RMP Page Found");
+        $(`#link${i}`).text("Add Listing").attr("href", link);
+      }
     }
-
-    //Continue if instructor has been seen before, or if no name present
-    if (searched.includes(instructorName) || !instructorName) {
-        return
-    } else {
-        //Keep track of instructors iterated over
-        searched.push(instructorName)
-    }
-
-    //Loading indicator
-    $(`#rating${i}`).text("Loading...")
-
-    //Typo check
-    if (typos.hasOwnProperty(instructorName)) {
-        instructorName = typos[instructorName]
-    }
-
-    chrome.runtime.sendMessage({isUBCO: isUBCO(), instructorName: instructorName }, function (result) {
-        const isSuccessful = result.isSuccessful
-        const rating = result.averageRatingScore
-        const link = result.link
-        const numRatings = result.numRatings
-
-        if (isSuccessful) {
-            //Hide loading indicator
-            $('.loader').hide()
-
-            $(`#rating${i}`).text(rating ? `Rating: ${rating} / 5` : 'N/A')
-            $(`#numRatings${i}`).text(`(${numRatings} ratings)`)
-            $(`#link${i}`).text(`RMP Page`).attr("href", link)
-        } else {
-            //Indicate instructor could not be found
-            $(`#rating${i}`).text("No RMP Page Found")
-            $(`#link${i}`).text('Add Listing').attr('href', 'https://www.ratemyprofessors.com/AddTeacher.jsp')
-        }
-    })
-
-})
+  );
+});
 
 function isUBCO() {
-    return $('.ubc7-campus').attr('id') === 'ubc7-okanagan-campus'
+  return $(".ubc7-campus").attr("id") === "ubc7-okanagan-campus";
 }
